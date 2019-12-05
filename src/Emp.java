29a195828541d8ef48e7f0d45609291cb61d47de
isse-a91 <isse-a91@142.132.136.121>
2019-12-05 13:01:17 -0600

    added setters and getters for employee properties

--- src/Emp.java
+++ src/Emp.java
@@ -1,3 +1,8 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /********************************************************************
  * Programmer:	Ayub Isse
  * Class:  CS30S
@@ -11,10 +16,14 @@
 
 public class Emp {
     //*** Class Variables ***
-        
+        public static int nextID = 1000;
     //*** Instance Variables ***    
      
-        private String strin = "";                                      // string data input from keyboard
+        private int id = 0;
+        private double hours = 0;
+        private double wage = 0;
+        
+        
         
     //*** Constructors ***
     
@@ -30,47 +39,124 @@ public class Emp {
     * ****************************************/
 
     /*****************************************
-    * Description: brief description of the methods purpose
+    * Description: Default constructor for employee
+    * 
+    * ****************************************/
+    public Emp(){
+        id = nextID++;
+    }
+    /*****************************************
+    * Description: Initialized Constructor
     * 
     * Interface:
     * 
-    * @param        each parameter of the method should be listed with an @param
-    * @param        parametername description of parameter
-    * 
-    * @return       any return value will be noted here
+    * @param        h int: hours
+    * @param        w double: wage
     * ****************************************/
-    public Emp(){
-        
-    }
+    public Emp(int h, double w){
+        id = nextID++;
+        hours = h;
+        wage = w;
+    }    
+           
     
     
     
     //*** Getters ***
     /*****************************************
-    * Description: Gets employee data from txt file
+    * Description: Gets employee id
     * 
     * Interface:
     * 
-    * @param        each parameter of the method should be listed with an @param
-    * @param        parametername description of parameter
-    * 
-    * @return       any return value will be noted here
+    * @return       id: int
     * ****************************************/
-    public static int getData(){
-        strin = fin.readLine();
-        while(strin != null){  
-            //System.out.println(strin);
-            
-            = Double.parseDouble(strin);     // putting input intp array
-  
-                strin = fin.readLine();                         // loop update
-            } // loop to get input and put into a list
-            return
-    } //  end getIntInput
+    public int getId(){
+        return this.id;
+    }
+    /*****************************************
+    * Description: Gets wage
+    * 
+    * Interface:
+    * 
+    * @return       Wage: double
+    * ****************************************/
+    public double getWage(){
+        return this.wage;
+    }
+    /*****************************************
+    * Description: Gets hours
+    * 
+    * Interface:
+    *
+    * @return       Hours: double
+    * ****************************************/
+    public double getHours(){
+        return this.hours;
+    }
+    /*****************************************
+    * Description: Get grosspay
+    * 
+    * Interface:
+    * 
+    * @return       Gross Pay: double
+    * ****************************************/
+    public double getGrossPay(){
+        return this.hours * wage;
+    }
+    /*****************************************
+    * Description: Gets pay without overtime
+    * 
+    * Interface:
+    * 
+    * @return       normal pay: double
+    * ****************************************/
+    public double getNormPay(){
+        if(hours <= 40){
+            return this.hours * wage;
+        }
+        else{
+            return 40 * wage;
         }
     }
-    
-    
+    /*****************************************
+    * Description: Gets overtime pay only
+    * 
+    * Interface:
+    * 
+    * @return       overtime pay: double
+    * ****************************************/
+    public double getOtPay(){
+        if(hours <= 40){
+            return 0;
+        }
+        else{
+            return (this.hours - 40) *  wage * 1.5;
+        }
+    }
     //*** Setters ***
+    
+    /*****************************************
+    * Description: Set hours
+    * 
+    * Interface: 
+    * 
+    * @param    x int: update for hours
+    ****************************************/
+    public void setHours(int x){
+        hours = x;
+    }
+    
+    /*****************************************
+    * Description:  Set wage
+    * 
+    * Interface:
+    * 
+    * 
+    *
+    * @param    x int: update for wage 
+    ****************************************/
+    public void setWage(int x){
+        wage = x;
+    } 
     
 } // end of public class
